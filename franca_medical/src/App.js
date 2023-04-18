9ce974a88cf61788ca20d978e0adf262cc2b332a
Danilo Ribeiro <92061792+dRibSouza@users.noreply.github.com>
2023-04-18 15:50:37 +0000

    homepage

---
+++ franca_medical/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="main">
+
+      </div>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
---
+++ franca_medical/src/components/Content.js
@@ -0,0 +1,29 @@
+import './Content.css'
+function Content() {
+    return (
+        <>
+            <div className="center_content">
+                <div className="conteudo">
+                    <ul id="navbar">
+                        <li>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus, enim et blandit dignissim, mauris ex pretium nulla, id ultrices ex purus eget est. Vivamus et massa pretium, consectetur ante eu, scelerisque nisl. Phasellus volutpat, ligula congue dapibus consequat, dui nulla iaculis orci, vitae aliquet justo sapien eu dui. Mauris dictum ante at cursus dictum. Donec volutpat mi id libero elementum finibus. Quisque ex dolor, tincidunt eget nulla et, lobortis eleifend turpis. Praesent non elementum eros, quis dictum leo.
+                            </p>
+                        </li>
+                        <li>
+                            <img src="Public health-amico 1.png" alt="vários pessoas"></img>
+                        
+                        </li>
+                        <li>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus, enim et blandit dignissim, mauris ex pretium nulla, id ultrices ex purus eget est. Vivamus et massa pretium, consectetur ante eu, scelerisque nisl. Phasellus volutpat, ligula congue dapibus consequat, dui nulla iaculis orci, vitae aliquet justo sapien eu dui. Mauris dictum ante at cursus dictum. Donec volutpat mi id libero elementum finibus. Quisque ex dolor, tincidunt eget nulla et, lobortis eleifend turpis. Praesent non elementum eros, quis dictum leo.
+                            </p>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+        </>
+        
+    )
+}
+export default Content
