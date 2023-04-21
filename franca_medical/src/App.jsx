101eff323e976f63ce7aeef8457c9fc6a32bca99
Danilo Ribeiro <92061792+dRibSouza@users.noreply.github.com>
2023-04-21 13:49:08 -0300

    Update App.jsx
    comentário sobre adicionar itens fixos em todas as páginas

--- franca_medical/src/App.jsx
+++ franca_medical/src/App.jsx
@@ -5,6 +5,7 @@ import { AppRoutes } from './routes';
 
 export const App= () => {
   return (
+    // Para fazer um header fixo para todos as páginas, podemos colocar ele aqui.<br/>
     <AppRoutes/>
   );
 }
