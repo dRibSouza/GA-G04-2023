66456f55c50333ea189c7a8145517b3af2f1e03a
Danilo Ribeiro <92061792+dRibSouza@users.noreply.github.com>
2023-04-21 15:01:00 -0300

    Update App.jsx

--- franca_medical/src/App.jsx
+++ franca_medical/src/App.jsx
@@ -5,7 +5,8 @@ import { AppRoutes } from './routes';
 
 export const App= () => {
   return (
-    // Para fazer um header fixo para todos as páginas, podemos colocar ele aqui.<br/>
+    // Para fazer um header fixo para todos as páginas, podemos colocar ele aqui.<br/> // correção ele vai na página de rotas ao invés daqui :[
+    
     <AppRoutes/>
   );
 }
